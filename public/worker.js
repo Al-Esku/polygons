cd17fa03643bdcfd0bfb0a39d2546c07acef2f0a
Al-Esku <alex.holton@yandex.com>
2025-01-19 03:26:24 +1300

    Adding workers to run multiple calculations at the same time

---
+++ public/worker.js
@@ -0,0 +1,187 @@
+function findLoops(lists, sum, sides) {
+    //console.log(lists)
+    if (lists.length === sides) {
+        //console.log("hi")
+        const loop = reorderListsToFormLoop(lists)
+        return loop.length !== 0 ? [loop]: []
+    } else {
+        const frequencyMap = new Map()
+        for (let i = 0; i < lists.length; i++) {
+            for (const num of lists[i]) {
+                const indices = frequencyMap.get(num) || []
+                frequencyMap.set(num, [...indices, i]);
+            }
+        }
+
+        if (!lists.every(list => partialListIsValid(list, frequencyMap))) {
+            //console.log(`${lists} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid`)
+            return []
+        }
+
+        let loops = []
+        for (let i = 1; i < 3; i++) {
+            findSums(lists[lists.length - 1][i], sum).forEach(option => {
+                loops = loops.concat(findLoops([...lists, option], sum, sides))
+            })
+        }
+        return loops
+    }
+}
+
+function partialListIsValid(list, frequencyMap) {
+    let duplicates = 0
+    let valid = true
+    for (const num of list) {
+        const indexes = frequencyMap.get(num)
+        if (indexes.length === 2) {
+            duplicates++
+        } else if (indexes.length >= 3) {
+            //console.log(`${list} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid: Too many indexes`)
+            valid = false
+        }
+        for (let i = 0; i < indexes.length - 1; i++) {
+            if (Math.abs(indexes[i] - indexes[i+1]) !== 1 && indexes[i+1] !== list.length - 1) {
+                //console.log(`${list} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid: Non-looping indexes`)
+                valid = false
+            }
+        }
+    }
+    return list.length === 3 && valid && (duplicates <= 2)
+}
+
+function listIsValid(list, frequencyMap) {
+    let duplicates = 0
+    let singles = 0
+    for (const num of list) {
+        if (frequencyMap.get(num)?.length === 2) {
+            duplicates++
+        } else if (frequencyMap.get(num)?.length === 1) {
+            singles++
+        }
+    }
+    return list.length === 3 && duplicates === 2 && singles === 1
+}
+
+function normalizeList(list, frequencyMap, last) {
+    const normalizedList = []
+    if (last === 0) {
+        const first = list.find((num) => {
+            return frequencyMap.get(num).length === 2
+        }) || -1
+        normalizedList.push(first)
+        const middle = list.find((num) => {
+            return frequencyMap.get(num).length === 1
+        }) || -1
+        normalizedList.push(middle)
+        const newLast = list.findLast((num) => {
+            return frequencyMap.get(num).length === 2
+        }) || -1
+        normalizedList.push(newLast)
+    } else {
+        if (list.includes(last)) {
+            normalizedList.push(last)
+            const middle = list.find((num) => {
+                return frequencyMap.get(num).length === 1
+            }) || -1
+            normalizedList.push(middle)
+            const newLast = list.findLast((num) => {
+                return frequencyMap.get(num).length === 2 && num !== last
+            }) || -1
+            normalizedList.push(newLast)
+        }
+    }
+    return normalizedList
+}
+
+function containsIdenticalList(
+    listOfLists,
+    targetList
+) {
+    // Normalize the target list by sorting its elements
+    const sortedTarget = [...targetList].sort((a, b) => a - b);
+
+    // Check each list in the list of lists
+    return listOfLists.some((list) => {
+        // Normalize the current list by sorting its elements
+        const sortedList = [...list].sort((a, b) => a - b);
+        // Compare the normalized list with the normalized target
+        return sortedList.length === sortedTarget.length &&
+            sortedList.every((value, index) => value === sortedTarget[index]);
+    });
+}
+
+function reorderListsToFormLoop(lists) {
+    const newList = []
+    const frequencyMap = new Map()
+
+    for (let i = 0; i < lists.length; i++) {
+        for (const num of lists[i]) {
+            const indices = frequencyMap.get(num) || []
+            frequencyMap.set(num, [...indices, i]);
+        }
+    }
+
+    if (!lists.every(list => listIsValid(list, frequencyMap))) {
+        //console.log(`${lists} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid`)
+        return []
+    }
+
+    let index = 0
+    let last = 0
+    while (newList.length < lists.length) {
+        const list = normalizeList(lists[index], frequencyMap, last)
+        if (list.length === 3 && list.every((num) => {
+            return num > 0
+        })) {
+            last = list[2]
+            const newIndex = frequencyMap.get(last)?.find((num) => {
+                return num !== index
+            })
+            if (newIndex !== undefined) {
+                index = newIndex
+            } else {
+                //console.log(`No valid index found for frequency map ${frequencyMap.get(last)} with index ${index}`)
+                return []
+            }
+        }
+        if (containsIdenticalList(newList, list)) {
+            //console.log(`${newList} already contains ${list}`)
+            return []
+        }
+        newList.push(list)
+    }
+    //console.log(`${newList} is valid`)
+    return newList
+}
+
+const findSums = ((num, sum) => {
+    const results = []
+    for (let i = 1; i < Math.floor((sum - num)/2) + 1; i++) {
+        if (new Set([num, i, sum - num - i]).size === 3) {
+            results.push([num, i, sum - num - i])
+        }
+    }
+    return results
+})
+
+
+const generatePolygons = ((high, sum, sides) => {
+    let tempPolygons = []
+    const lines = findSums(high, sum)
+    //console.log(lines)
+    if (lines.length === 0) {
+        return tempPolygons
+    } else {
+        for (const line of lines) {
+            tempPolygons = tempPolygons.concat(findLoops([line], sum, sides))
+        }
+    }
+    return tempPolygons
+})
+
+self.addEventListener("message", function(message) {
+    const newPolygons = generatePolygons(message.data.high, message.data.sum, message.data.sides)
+    //console.log(tempPolygons)
+    self.postMessage(newPolygons)
+    self.close()
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
     const [sum, setSum] = React.useState(9);
     const [polygons, setPolygons] = React.useState<number[][][]>([])
     const [time, setTime] = React.useState(0)
+    const [loading, setLoading] = React.useState(false)
 
     const points = ((radius: number) => {
         const angle = 360 / sides
@@ -70,172 +71,6 @@ export default function Home() {
 
 
     React.useEffect(() => {
-        function findLoops(lists: number[][]) {
-            //console.log(lists)
-            if (lists.length === sides) {
-                //console.log("hi")
-                const loop = reorderListsToFormLoop(lists)
-                return loop.length !== 0 ? [loop]: []
-            } else {
-                const frequencyMap = new Map<number, number[]>()
-                for (let i = 0; i < lists.length; i++) {
-                    for (const num of lists[i]) {
-                        const indices = frequencyMap.get(num) || []
-                        frequencyMap.set(num, [...indices, i]);
-                    }
-                }
-
-                if (!lists.every(list => partialListIsValid(list, frequencyMap))) {
-                    //console.log(`${lists} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid`)
-                    return []
-                }
-
-                let loops: number[][][] = []
-                for (let i = 1; i < 3; i++) {
-                    findSums(lists[lists.length - 1][i]).forEach(option => {
-                        loops = loops.concat(findLoops([...lists, option]))
-                    })
-                }
-                return loops
-            }
-        }
-
-        function partialListIsValid(list: number[], frequencyMap: Map<number, number[]>) {
-            let duplicates = 0
-            let valid = true
-            for (const num of list) {
-                const indexes = frequencyMap.get(num)
-                if (indexes?.length === 2) {
-                    duplicates++
-                } else if (indexes!.length >= 3) {
-                    //console.log(`${list} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid: Too many indexes`)
-                    valid = false
-                }
-                for (let i = 0; i < indexes!.length - 1; i++) {
-                    if (Math.abs(indexes![i] - indexes![i+1]) !== 1 && indexes![i+1] !== list.length - 1) {
-                        //console.log(`${list} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid: Non-looping indexes`)
-                        valid = false
-                    }
-                }
-            }
-            return list.length === 3 && valid && (duplicates <= 2)
-        }
-
-        function listIsValid(list: number[], frequencyMap: Map<number, number[]>) {
-            let duplicates = 0
-            let singles = 0
-            for (const num of list) {
-                if (frequencyMap.get(num)?.length === 2) {
-                    duplicates++
-                } else if (frequencyMap.get(num)?.length === 1) {
-                    singles++
-                }
-            }
-            return list.length === 3 && duplicates === 2 && singles === 1
-        }
-
-        function normalizeList(list: number[], frequencyMap: Map<number, number[]>, last: number) {
-            const normalizedList = []
-            if (last === 0) {
-                const first = list.find((num) => {
-                    return frequencyMap.get(num)!.length === 2
-                }) || -1
-                normalizedList.push(first)
-                const middle = list.find((num) => {
-                    return frequencyMap.get(num)!.length === 1
-                }) || -1
-                normalizedList.push(middle)
-                const newLast = list.findLast((num) => {
-                    return frequencyMap.get(num)!.length === 2
-                }) || -1
-                normalizedList.push(newLast)
-            } else {
-                if (list.includes(last)) {
-                    normalizedList.push(last)
-                    const middle = list.find((num) => {
-                        return frequencyMap.get(num)!.length === 1
-                    }) || -1
-                    normalizedList.push(middle)
-                    const newLast = list.findLast((num) => {
-                        return frequencyMap.get(num)!.length === 2 && num !== last
-                    }) || -1
-                    normalizedList.push(newLast)
-                }
-            }
-            return normalizedList
-        }
-
-        function containsIdenticalList(
-            listOfLists: number[][],
-            targetList: number[]
-        ): boolean {
-            // Normalize the target list by sorting its elements
-            const sortedTarget = [...targetList].sort((a, b) => a - b);
-
-            // Check each list in the list of lists
-            return listOfLists.some((list) => {
-                // Normalize the current list by sorting its elements
-                const sortedList = [...list].sort((a, b) => a - b);
-                // Compare the normalized list with the normalized target
-                return sortedList.length === sortedTarget.length &&
-                    sortedList.every((value, index) => value === sortedTarget[index]);
-            });
-        }
-
-        function reorderListsToFormLoop(lists: number[][]): number[][] {
-            const newList = []
-            const frequencyMap = new Map<number, number[]>()
-
-            for (let i = 0; i < lists.length; i++) {
-                for (const num of lists[i]) {
-                    const indices = frequencyMap.get(num) || []
-                    frequencyMap.set(num, [...indices, i]);
-                }
-            }
-
-            if (!lists.every(list => listIsValid(list, frequencyMap))) {
-                //console.log(`${lists} with frequency map ${JSON.stringify(Array.from(frequencyMap))} is invalid`)
-                return []
-            }
-
-            let index = 0
-            let last = 0
-            while (newList.length < lists.length) {
-                const list = normalizeList(lists[index], frequencyMap, last)
-                if (list.length === 3 && list.every((num) => {
-                    return num > 0
-                })) {
-                    last = list[2]
-                    const newIndex = frequencyMap.get(last)?.find((num) => {
-                        return num !== index
-                    })
-                    if (newIndex !== undefined) {
-                        index = newIndex
-                    } else {
-                        //console.log(`No valid index found for frequency map ${frequencyMap.get(last)} with index ${index}`)
-                        return []
-                    }
-                }
-                if (containsIdenticalList(newList, list)) {
-                    //console.log(`${newList} already contains ${list}`)
-                    return []
-                }
-                newList.push(list)
-            }
-            //console.log(`${newList} is valid`)
-            return newList
-        }
-
-        const findSums = ((num: number) => {
-            const results = []
-            for (let i = 1; i < Math.floor((sum - num)/2) + 1; i++) {
-                if (new Set([num, i, sum - num - i]).size === 3) {
-                    results.push([num, i, sum - num - i])
-                }
-            }
-            return results
-        })
-
         function addUniquePolygons(
             existingPolygons: number[][][],
             newPolygons: number[][][]
@@ -262,46 +97,53 @@ export default function Home() {
             return existingPolygons;
         }
 
-        const generatePolygons = ((high: number) => {
-            let tempPolygons: number[][][] = []
-            const lines = findSums(high)
-            //console.log(lines)
-            if (lines.length === 0) {
-                return tempPolygons
-            } else {
-                for (const line of lines) {
-                    tempPolygons = tempPolygons.concat(findLoops([line]))
-                }
-            }
-            return tempPolygons
-        })
+        function createWorker(high: number) {
+            return new Promise<number[][][]>(function(resolve, reject) {
+                const worker = new Worker("worker.js")
+                worker.addEventListener("message", function (message: {data: number[][][]}) {
+                    resolve(message.data)
+                })
+                worker.addEventListener("error", reject)
+                worker.postMessage({
+                    sum: sum,
+                    sides: sides,
+                    high: high
+                })
+            })
+        }
 
         const startTime = performance.now()
-        let tempPolygons: number[][][] = []
-        let high = sum - 3
-        while (high > Math.floor(sum / 3)) {
-            const newPolygons = generatePolygons(high)
-            tempPolygons = addUniquePolygons(tempPolygons, newPolygons)
-            high--
+        setLoading(true)
+        const promises = []
+        for (let high = sum - 3; high >= sides * 2; high--) {
+            promises.push(createWorker(high))
         }
-        //console.log(tempPolygons)
-        setPolygons(tempPolygons)
-        const endTime = performance.now()
-        setTime(endTime - startTime)
+
+        Promise.all(promises).then(function(data) {
+            setPolygons(data.reduce((acc, value) => {return addUniquePolygons(acc, value)}, [] as number[][][]))
+            const endTime = performance.now()
+            setTime(endTime - startTime)
+            setLoading(false)
+        }).catch(() => {
+            setPolygons([])
+            const endTime = performance.now()
+            setTime(endTime - startTime)
+            setLoading(false)
+        })
     }, [sum, sides])
 
     return (
         <div>
             <label>Sides:</label>
-            <input className={"ml-2 px-1 mr-6 border border-black"} value={sides} type={"number"} onInput={event => setSides(+event.currentTarget.value)}/>
+            <input disabled={loading} className={"ml-2 px-1 mr-6 border border-black"} value={sides} type={"number"} onInput={event => setSides(+event.currentTarget.value)}/>
             <label>Sum:</label>
-            <input className={"ml-2 px-1 mr-6 border border-black"} value={sum} type={"number"} onInput={event => setSum(+event.currentTarget.value)}/>
+            <input disabled={loading} className={"ml-2 px-1 mr-6 border border-black"} value={sum} type={"number"} onInput={event => setSum(+event.currentTarget.value)}/>
             <div>
                 Polygons: Found {polygons.length} in {time.toFixed(2)} milliseconds
             </div>
             <div className={"flex flex-wrap"}>
                 {polygons.map((poly, index) => {
-                    console.warn(poly)
+                    //console.warn(poly)
                     return <div key={index} id={index.toString()}>
                         <svg viewBox={`0 0 350 350`} className={`w-[350px] h-[350px]`}>
                             <polygon points={polygon().join(" ")} stroke={"black"} fill={"none"}/>
